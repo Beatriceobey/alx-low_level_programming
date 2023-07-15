beeb56afce99a080247180abfd7586e53d89a68e
Beatriceobey <beatriceobey210@gmail.com>
2023-07-15 01:44:43 -0700

    0x00-hello_world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
-#!/bin/bash
 #include<stdio.h>
 
 /**
-*main - Entry point
+*main - A C program that prints with printf
 *
-*Return: Always 0 (Success)
+*Return: 0 (Success)
 */
 int main(void)
 {
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,16 @@
-#!/bin/bash
-#include<stdio.h>
+#include <stdio.h>
 
 /**
-*main - Entry point
+*main - A C program that prints the size of various types of computer it is compiled or run on
 *
-*Return: Always 0 (Success)
+*Return: 0 (Success)
 */
 int main (void)
 {
-	printf(“Size of a char: %d byte(s)\n”, sizeof(char));
-	printf(“Size of an int: %d byte(s)\n”, sizeof(int));
-	printf(“Size of a long int: %d byte(s)\n”, sizeof(long int));
-	printf(“Size of a long long int: %d byte(s)\n”, sizeof(long long int));
-	printf(“Size of a float: %d byte(s)\n”, sizeof(float));
+	printf(“Size of a char: %1u byte(s)\n”, sizeof(char));
+	printf(“Size of an int: %1u byte(s)\n”, sizeof(int));
+	printf(“Size of a long int: %1u byte(s)\n”, sizeof(long int));
+	printf(“Size of a long long int: %1u byte(s)\n”, sizeof(long long int));
+	printf(“Size of a float: %1u byte(s)\n”, sizeof(float));
 	return (0);
 }
