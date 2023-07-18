e9968f24a6433b4ef1448ea2c1eaef9e5e46e953
Beatriceobey <beatriceobey210@gmail.com>
2023-07-18 15:02:50 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - Use putchar function only (twice)
+*description: print lowercase alphbets using putchar
+*
+* Return: 0
+*/
+int main(void)
+{
+	char lowercase;
+
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+{
+	putchar(lowercase);
+	putchar('\n');
+}
+	return (0);
+}
