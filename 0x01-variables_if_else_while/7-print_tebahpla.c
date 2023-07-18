49518a2be02b43c21a7872bc8f1c8bd7ebb72fa4
Beatriceobey <beatriceobey210@gmail.com>
2023-07-18 16:26:30 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - Print alphabets using putchar
+*description: Print lowercase backwords using putchar
+*
+*Return: 0
+*/
+int main(void)
+{
+	char lowercase;
+
+	for (lowercase = 'a'; lowercase => 'z'; lowercase++)
+{
+	putchar(lowercase);
+}
+	putchar('\n');
+	return (0);
+}
