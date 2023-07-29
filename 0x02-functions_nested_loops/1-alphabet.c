51463f5bfe7db065bfb97ecba01172f4d11750bf
Beatriceobey <beatriceobey210@gmail.com>
2023-07-29 08:21:36 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- * main - check the code
- *description: prints the alphabet, in lowercase, followed by a new line.
+ *print_alphabet - prints the alphabet, in lowercase, followed by a new line
  * Return: Always 0
  */
 void print_alphabet(void);
@@ -9,8 +8,7 @@ void print_alphabet(void);
 	int i;
 
 	for (i = 'a'; i <= 'z'; i++)
-	{
 		_putchar(i);
-	}
+
 	_putchar('\n');
 }
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,22 +1,6 @@
 #include "main.h"
 /**
  *print_last_digit - Write a function that prints the last digit of a number
- *@lowercase: int to check
  *Return: 0
  */
-int print_last_digit(int lowercase)
-{
-	int lowercase;
-
-	lowercase = n % 10;
-	if (lowercase < 0)
-	{
-		_putchar(-lowercase + 48);
-		return (-lowercase);
-	}
-	else
-	{
-		_putchar(lowercase + 48);
-		return (lowercase);
-	}
-}
+int print_last_digit(int)
